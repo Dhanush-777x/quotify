b078a8afc99274213497e9cb1e706dbacc85db76
dhanushsm <dhanushmani1504@gmail.com>
2024-11-23 19:24:40 +0530

    fix: quotes page

--- rbac-ui/src/components/Quotes.jsx
+++ rbac-ui/src/components/Quotes.jsx
@@ -42,7 +42,7 @@ const Quotes = () => {
       axios({
         method: "GET",
         url: `https://api.api-ninjas.com/v1/quotes?category=${quoteCategory}`,
-        headers: { "X-Api-Key": "vYYy1tycD6Zfs/4QAcZ81w==WOSooei6SHyhvBFn" },
+        headers: { "X-Api-Key": process.env.API_KEY },
       })
         .then((response) => {
           console.log("Quotes API Response:", response.data);
